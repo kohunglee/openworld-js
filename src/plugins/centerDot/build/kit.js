5164f120359bead6e72aa97228c3d2a1e036ef60
kohunglee <2528852314@qq.com>
2025-09-05 21:46:16 +0800

    暂时妥协，目录待优化

--- src/plugins/centerDot/build/kit.js
+++ src/plugins/centerDot/build/kit.js
@@ -48,6 +48,14 @@ export default function(ccgxkObj) {
             const list = obj.sound;
             obj.audio(list[G.musicMap[myevent]]);
         },
+
+        // 辅助函数，批量设置 EdiArgsInput 的 number 的 step
+        setInputsStep : (stepValue) => {
+            const EdiArgsInput = document.querySelectorAll('.EdiArgsInput');  // 那一大堆 OBJ 属性框
+            EdiArgsInput.forEach(input => {
+                input.step = stepValue;
+            })
+        },
     };
 
     ccgxkObj.centerDot.init = {...G, ...ccgxkObj.centerDot.init};
