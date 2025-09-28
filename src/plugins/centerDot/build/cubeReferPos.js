0736f302af60b6064751a74fa51744491dfa2ac4
kohunglee <2528852314@qq.com>
2025-09-28 19:52:53 +0800

    左键这个搞了

--- src/plugins/centerDot/build/cubeReferPos.js
+++ src/plugins/centerDot/build/cubeReferPos.js
@@ -11,11 +11,25 @@ export default function(ccgxkObj) {
         newCubePosType : 0,  // 默认模式为 关闭
         displayRefer: () => {
             const G = ccgxkObj.centerDot.init;
+            if(!G?.addCubeByMouseEvent) {
+                G.addCubeByMouseEvent = ()=>{
+                    if(G.newCubePosType !== 0){
+                        G.operaCube(1, true);  // 添加方块
+                        G.displayRefer();      // 关闭参考模型
+                    }
+                }
+                document.addEventListener('click', function(event, gObj = G) {
+                    if (event.button === 0) {  // 0 表示鼠标左键
+                        gObj.addCubeByMouseEvent();
+                    }
+                });
+            }
             const W = ccgxkObj.W;
             const totalModes = 2; // 总共有 2 个模式 (0, 1)
             G.newCubePosType = (G.newCubePosType + 1) % totalModes;
             switch (G.newCubePosType) {
                 case 1:  // 正交模式
+                    ccgxkObj.notCenterDot = true;  // 禁用【中心点插件】类内容
                     W.cube({
                         g: 'mainPlayer',
                         n: CUBE_NAME,
@@ -23,6 +37,14 @@ export default function(ccgxkObj) {
                         w: 1, h: 1, d: 1,
                         b: '#bbbbbb46',
                     });
+
+                    const mVP = ccgxkObj.mainVPlayer;
+                    const new_ry = ((mVP.rX)? 1: -1) * mVP.rY;
+                    W.cube({
+                        n: CUBE_NAME,
+                        ry: new_ry,  // 让方块的旋转角度始终以正交模式添加
+                    });
+
                     ccgxkObj.hooks.on('mouseMove', function(o, e, mode = G.newCubePosType, obj = ccgxkObj){
                         if(mode === 1){
                             const mVP = obj.mainVPlayer;
@@ -33,8 +55,14 @@ export default function(ccgxkObj) {
                             });
                         }
                     });
+                    // document.addEventListener('click', function(event) {
+                    //     if (event.button === 0) {  // 0 表示鼠标左键
+                    //         console.log('鼠标左键被点击了！');
+                    //     }
+                    // });
                     break;
                 case 0:  // 关闭模式
+                    ccgxkObj.notCenterDot = false;  // 重新启用【中心点插件】类内容
                     W.delete(CUBE_NAME);
                     break;
             }
