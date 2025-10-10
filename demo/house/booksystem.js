ad2bc3c521da27a3d52d6965f0b9fb68f6fecb25
kohunglee <2528852314@qq.com>
2025-10-10 20:45:30 +0800

    初步将两层的字给搞上去了

--- demo/house/booksystem.js
+++ demo/house/booksystem.js
@@ -3,7 +3,7 @@ function bookSystem(){  // 书 系统
     D.book = {};  // 初始化 书 容器
     const bookDataIns = [];
 
-    const shelfDefs = [  // 书格规则表
+    const shelfDefs = [  // 书格规则表，以 103 为基准
         { id: 30102, ref: 103, off: [-0.314, 'y'] },
         { id: 30101, ref: 'n30102', off: [1.28, 'z'] },
         { id: 30103, ref: 'n30102', off: [-1.22, 'z', -0.09, 'y'], maxlen: 1.08, count: 30 },
@@ -49,6 +49,16 @@ function bookSystem(){  // 书 系统
 
     ];
 
+    const fix3 = v => Number(v.toFixed(3));  // 保留 3 位小数
+
+    const colors = [  // 书脊颜色 库
+        '#A59A8C',
+        '#8E8E88',
+        '#A8AEB5',
+        '#9B928A',
+        '#7F8682',
+        '#8C8C8C'
+    ];
 
     // 生成书格数据
     function fillBooks(baseBook, totalLength = 1.05, count = 30) {
@@ -58,18 +68,14 @@ function bookSystem(){  // 书 系统
         const seed = baseBook?.seed || 1;  // 随机数种子
         const random = k.genPR(seed, ks * count );
         const baseBottomY = baseBook.y - baseBook.h / 2;  // 计算书的底面 Y 坐标（底边固定）
-        // const rawDepths = Array.from({ length: count }, () =>
-        //     baseBook.d * (0.9 + Math.random() * 0.8) // 深度变化 ±40% （书脊宽）
-        // );
         const rawDepths = Array.from({ length: count }, (_, i) =>
-            baseBook.d * (0.9 + (1 - random[i * ks + 1]) * 0.8) // 深度变化 ±40%
+            baseBook.d * (0.9 + (1 - random[i * ks + 1]) * 0.8) // 深度变化 ±40%（书脊宽）
         );
         const rawSum = rawDepths.reduce((a, b) => a + b, 0);
         const scale = totalLength / rawSum;
         const depths = rawDepths.map(d => d * scale);
         for (let i = 0; i < count; i++) {
             const d = depths[i];
-            // const w = baseBook.w * (0.7 + random[i * ks] * 0.3); // 深度变化 ±10%（书脊对齐）
             const w = baseBook.w; // 深度变化 ±10%（书脊对齐）
             const h = baseBook.h * (0.9 + random[i * ks + 1] * 0.1); // 高度变化 ±20%
             if (i > 0) {
@@ -77,23 +83,10 @@ function bookSystem(){  // 书 系统
                 currentZ = prev.z + (prev.d / 2 + d / 2) + 0.0025;
             }
             const y = baseBottomY + h / 2;  // 调整中心 y，让底面固定
-            const colors = [
-                '#A59A8C', // 咖啡灰 —— 底色稳定
-                '#8E8E88', // 铁灰 —— 提供视觉重心
-                '#A8AEB5',  // 石墨蓝灰 —— 提供视觉重量
-                '#9B928A', // 淡褐灰 —— 稍带暖感，接近书脊旧化色
-                '#7F8682', // 炭灰 —— 视觉锚点
-                '#8C8C8C'  // 中灰 —— 平衡整体明暗
-            ];
-
             const color = colors[Math.floor(random[i * ks + 2] * colors.length)];
             books.push({
-                x: baseBook.x,
-                y,
-                z: currentZ,
-                w,
-                h,
-                d,
+                x: fix3(baseBook.x), y: fix3(y), z: fix3(currentZ),
+                w: fix3(w), h: fix3(h), d: fix3(d),
                 b: color,
             });
         }
@@ -108,22 +101,22 @@ function bookSystem(){  // 书 系统
         const base = typeof ref === 'number'
             ? [ref]
             : [...D.book[ref]];
-        D.book[`n${id}`] = offset(base, off[0], 2, off[1], off[2], off[3]);  // 偏移
-        const firstBook = cubeDatas[D.book[`n${id}`][0]];  // 得到左侧第一本书的数据
+        D.book[`n${id}`] = offset(base, off[0], 2, off[1], off[2], off[3]);  // 偏移操作，生成新书格，第一本书
+        const firstBook = cubeDatas[D.book[`n${id}`][0]];  // 得到 新书格 第一本书的数据
         const bookSet = fillBooks({ ...firstBook, seed: id }, maxlen, count);  // 生成整格数据
         for (const book of bookSet) {  // 推入数据流
             cubeDatas.push({ ...book, st: 1, iv: true, unIns:1, }); // st:1 表示静态书, unIns:1 代表不统一实例化
             bookDataIns.push({ ...book, st: 1 }); // st:1 表示静态书
         }
+        bookDataIns.push({del:1}); // 方便检索数据，按书格添加 del
     }
-    
 
     for (const key in D.book) {  //+ 删除用于定位的第一本书
         const firstBookIndex = D.book[key][0];
         cubeDatas[firstBookIndex] = {del : 1};
     }
-    // cubeDatas[103] = {del : 1};
-    cubeDatas[102] = {del : 1};
+    cubeDatas[103] = {del : 1};  // 书架定位书
+    cubeDatas[102] = {del : 1};  // 辅助 svg 片片儿 定位 模型
 
     k.testInsData = bookDataIns;
     k.W.cube({  // 渲染实例化
@@ -133,60 +126,111 @@ function bookSystem(){  // 书 系统
         mix: 0.8,
     });
 
-    // console.log(testimg);
-
-    // k.W.cube({  // 渲染实例化
-    //     n: 'testInsPlane',
-    //     x: 47.073, y: 1.75, z: -22.555,
-    //     w: 1.19, h: 0.83, ry: 0,
-    //     t: testimg,
-    //     mix: 1,
-    // });
-
     /**** ---- 【实验 svg】 ------ */
-    // svg test
-    const svgTestCode = `
+
+    console.time('svg');
+
+    const pool = "憨狗天地玄黄宇宙洪荒日月盈AabcdeFGHIJ昃辰宿列张寒来暑往秋收冬藏";
+    const poolLen = pool.length;
+    function randCN() {
+        let len = (Math.random() * 5 | 0) + 4;
+        let s = '';
+        for (let i = 0; i < len; i++) s += pool[Math.random() * poolLen | 0];
+        return s;
+    }
+
+    // for (let i = 0; i < 681; i++) {  // PS 上层 681 个
+    //     if(bookDataIns[i]?.z === undefined){
+    //         continue;
+    //     }
+    //     const x = fix3(bookDataIns[i].z - (-23.116)) * 1000 ;
+    //     const y = fix3(bookDataIns[i].y - 2.898) * 1000;
+    //     myTextCode += `<text x="${(60 + x + 10) * svgClearVal}" y="${(170 - y  + 20) * svgClearVal}">${randCN()}</text>`;
+    // }
+
+    const svgTextCodeBuild = (x, y, w_z, w_y, data) => {
+        let textCode = '';
+        for (let i = 0; i < data.length; i++) {
+            const x1 = fix3(data[i].z - w_z) * 1000 ;
+            const y1 = fix3(data[i].y - w_y) * 1000;
+            textCode += `<text x="${(x + x1 + 10) * svgClearVal}" y="${(y - y1  + 20) * svgClearVal}">${randCN()}</text>`;
+        }
+        return textCode;
+    }
+
+        const svgCodeMake = (width, height, textCode) => {
+
+            return svgTestCode = `
     <svg xmlns="http://www.w3.org/2000/svg"
-     viewBox="0 0 1190 840"
-     width="1190" height="840"
-     preserveAspectRatio="xMidYMid meet"
-     role="img"
-     aria-label="三个红色方块 与 你好啊">
+        viewBox="0 0 ${width} ${height}"
+        width="${width}" height="${height}"
+        preserveAspectRatio="xMidYMid meet"
+        role="img"
+        aria-label="三个红色方块 与 你好啊" style="width:500px">
+        <rect x="0" y="0" width="100%" height="100%" fill="#00000000"/>
+        <rect x="60"  y="30" width="40" height="40" fill="#e63946" rx="4"/>
+        <rect x="110" y="20" width="40" height="80" fill="#e63946" rx="4"/>
+        <rect x="160" y="35" width="40" height="50" fill="#e63946" rx="4"/>
 
-  <!-- 背景，全覆盖 -->
-  <rect x="0" y="0" width="100%" height="100%" fill="#00000000"/>
+        <g font-family=" 'Noto Sans SC', 'PingFang SC', 'Microsoft Yahei', 'Heiti SC', 'Source Han Sans SC', sans-serif "
+            font-size="${15 * svgClearVal}"
+            fill="#000000ff"
+            writing-mode="vertical-rl"
+            text-orientation="upright"
+            text-anchor="start"
+            dominant-baseline="hanging">
 
-  <!-- 三个红色方块 -->
-  <rect x="60"  y="30" width="40" height="40" fill="#e63946" rx="4"/>
-  <rect x="110" y="20" width="40" height="80" fill="#e63946" rx="4"/>
-  <rect x="160" y="35" width="40" height="50" fill="#e63946" rx="4"/>
-  
-    <text x="450" y="270"
-        font-family="'Noto Sans SC', 'PingFang SC', 'Microsoft Yahei', 'Heiti SC', 'Source Han Sans SC', sans-serif"
-        font-size="20"
-        fill="#000"
-        text-anchor="middle"
-        writing-mode="vertical-rl"
-        glyph-orientation-vertical="0">
-    水调歌头
-  </text>
-</svg>
-    `;
+            <text x="60" y="170">水调歌头</text>
+            ${textCode}
+        </g>
+    </svg>
+            `;
+
+    }
+
+    const svgClearVal = 1;  // 清晰度
+    const up_TextCode = svgTextCodeBuild(60, 170, -23.116, 2.898, bookDataIns.slice(0, 681));  // 上层 681 个
+    const down_TextCode = svgTextCodeBuild(77, 282, -23.116, 1.853, bookDataIns.slice(681, bookDataIns.length));  // 下层 其余的
+    const upSvg = svgCodeMake(7400 * svgClearVal, 940 * svgClearVal, up_TextCode);  // 上层的 SVG 数据
+    const downSvg = svgCodeMake(7400 * svgClearVal, 935 * svgClearVal, down_TextCode);  // 下层的 SVG 数据
+
     const textureAlp = [
-        { id:'svgTest', type: 'svg', svgCode: svgTestCode },
+        { id:'upSvgPng', type: 'svg', svgCode: upSvg },
+        { id:'downSvgPng', type: 'svg', svgCode: downSvg },
     ];
     k.loadTexture(textureAlp).then(loadedImage => {
-        const mySvg = k.textureMap.get('svgTest');
-        k.W.plane({
-            n: 'testPng',
-            // x: 47.0739,
-            x: 47.073, y: 1.75, z: -22.555,
-            w: 1.19, h: 0.84, ry: -90,
-            t:mySvg,
-        });
-    })
-    /**** ---------- */
-}
+        const upSvgPng = k.textureMap.get('upSvgPng');
+        const downSvgPng = k.textureMap.get('downSvgPng');
 
+        k.W.plane({  // 上大书架
+            n: 'n010101',
+            x: 47.373, y: 2.681, z: -19.478,
+            w: 7.4, h: 0.94, ry: -90,
+            t: upSvgPng,
+        });
+
+        k.W.plane({  // 下小书架
+            n: 'n010102',
+            x: 47.072, y: 1.75, z: -19.497,
+            w: 7.4, h: 0.935, ry: -90,
+            t: downSvgPng,
+        });
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+    /**** ---------- */
+    console.timeEnd('svg');
+}
 
 
