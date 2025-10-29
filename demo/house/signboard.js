36d40e782d15fc2ea6c8d8deea58091073995ca9
kohunglee <2528852314@qq.com>
2025-10-29 23:26:37 +0800

    指示板添加到一半了

---
+++ demo/house/signboard.js
@@ -0,0 +1,31 @@
+const singboard = {
+
+    setTest : () => {
+        // const posData = {
+        //     x: 16.712,
+        //     y: 2.3,
+        //     z: -35.212,
+        //     ry: 90,
+        //     w: 2,
+        //     h: 1.5,
+        // }
+
+        // k.W.plane({
+        //     n: 'testplane',
+        //     ...posData,
+        // });
+
+        k.addTABox({
+            DPZ : 3,
+            X: 16.712,
+            Y: 2.3,
+            Z: -35.212,
+            rY: 90,
+            width : 2,
+            height: 1.5,
+            shape: 'plane',
+            isPhysical: false,
+        });
+    }
+
+}
