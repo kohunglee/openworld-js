1633c19771abb36e6b0632e6baf20cf65b4acd18
kohunglee <2528852314@qq.com>
2025-11-07 21:25:00 +0800

    test

--- demo/house/dog.js
+++ demo/house/dog.js
@@ -10,6 +10,6 @@ const dog = {
 
         console.log('dof ok');// const dog = new Dog();
 
-        k.W.wolf_txt({n:'dog', x: 30, y: 1.5, z: -30, size: 10,s:1, ry:90})
+        k.W.wolf_txt({n:'dog', x: 30, y: 1.5, z: -30, size: 10,s:1, t:dls, ry:-90, b:"#eeff00ff", mix:0.5})
     },
 }
