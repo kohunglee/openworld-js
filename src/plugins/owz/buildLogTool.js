5c92dc0161208618969b1ae1e471ec1329a0cb83
kohunglee <2528852314@qq.com>
2025-11-20 17:03:19 +0800

    feat: building Code temporarily separated
    暂时把 图书馆 的代码给抽离出来了，开始研究怎么能软删除。

---
+++ src/plugins/owz/buildLogTool.js
@@ -0,0 +1,86 @@
+/**
+ * 建造模型时，使用的 偏移、阵列工具
+ * ========
+ * 四个逻辑函数：symopera、offsetopera ，
+ */
+export default class Player {
+  constructor(){}
+}
+
+
+export default function(ccgxkObj){
+
+    // if(k.notSymOff) return 0;
+    //     var orig_data = cubeDatas[items];
+    //     var agent = {...orig_data};
+    //     for (const axis of ["x", "y", "z"]) {
+    //         if (axes[axis] !== undefined) {
+    //             agent[axis] -= (orig_data[axis] - axes[axis]) * 2;
+    //             const rot = (axis === 'z') ? 'x' : 'z';
+    //             if(agent['r' + rot]){
+    //                 agent['r' + rot] = - orig_data['r' + rot];
+    //             }
+    //         }
+    //     }
+    //     return cubeDatas.push(agent);
+    // }
+
+    // globalThis.offsetopera = (items, distance, times = 0, axes = 'x', distance2, axes2, distance3, axes3) => {  // 偏移操作
+    //     if(k.notSymOff) return 0;
+    //     var orig_data = cubeDatas[items];
+    //     var agent = {...orig_data};
+    //     for (const axis of ["x", "y", "z"]) {
+    //         if (axes === axis) {
+    //             agent[axis] -= distance * times;
+    //         }
+    //     }
+    //     if(distance2) {
+    //         for (const axis of ["x", "y", "z"]) {
+    //             if (axes2 === axis) {
+    //                 agent[axis] -= distance2 * times;
+    //             }
+    //         }
+    //     }
+    //     if(distance3) {
+    //         for (const axis of ["x", "y", "z"]) {
+    //             if (axes3 === axis) {
+    //                 agent[axis] -= distance3 * times;
+    //             }
+    //         }
+    //     }
+    //     return cubeDatas.push(agent);
+    // }
+    
+    // globalThis.symo = (items, axes = {}) => {  // 对称数组内的物体
+    //     const addInfo = [];
+    //     for (const it of items) {
+    //         if(it === -1) continue;
+    //         if (Array.isArray(it)) {
+    //             for (let n = it[0]; n <= it[1]; n++) {
+    //                 addInfo.push(symopera(n, axes) - 1); 
+    //             }
+    //         } else {
+    //             addInfo.push(symopera(it, axes) - 1);
+    //         }
+    //     }
+    //     return addInfo;
+    // }
+    
+    // globalThis.offset = (items, distance, times, axes, distance2, axes2, distance3, axes3) => {  // 偏移数组内的物体
+    //     const addInfo = [];
+    //     for (let index = 1; index < times; index++) {  // 偏移
+    //         for (const it of items) {
+    //             if(it === -1) continue;
+    //             if (Array.isArray(it)) {
+    //                 for (let n = it[0]; n <= it[1]; n++) {
+    //                     addInfo.push(offsetopera(n, distance, index, axes, distance2, axes2, distance3, axes3) - 1);
+    //                 }
+    //             } else {
+    //                 addInfo.push(offsetopera(it, distance, index, axes, distance2, axes2, distance3, axes3) - 1);
+    //             }
+    //         }
+    //     }
+    //     return addInfo;
+    // }
+    
+}
