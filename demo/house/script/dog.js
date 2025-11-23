d3a60977b1957341a6c619b4219ff2370890a978
kohunglee <2528852314@qq.com>
2025-11-23 16:18:04 +0800

    feat: add some phycial cube
    加了几个物理块儿，闲着无事

--- demo/house/script/dog.js
+++ demo/house/script/dog.js
@@ -43,5 +43,23 @@ const dog = {
                 z: 0,
             }
         });
+
+        for (let i = 0; i < 20; i++) {
+            // 添加一个小方块
+            k.addTABox({
+                DPZ : 1,
+                isPhysical: true,
+                mass: 10,
+                backgroundColor: '#ff0000ff',
+                texture: dls,
+                mixValue: 0.5,
+                colliGroup: 1,
+                X: 28 + Math.random() * 10,
+                Y: 6,
+                Z: -3 + Math.random() * 4,
+                size: 0.8,
+            });
+        }
+
     },
 }
