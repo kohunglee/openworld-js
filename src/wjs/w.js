241ee80bf5d445c47abc1e5c7309c0aa324c6190
kohunglee <2528852314@qq.com>
2025-11-25 00:50:08 +0800

    feat: sky box
    初步搞了搞 天空盒，明天继续研究有用的

--- src/wjs/w.js
+++ src/wjs/w.js
@@ -574,7 +574,7 @@ W.add("pyramid", {
     for(i = 0; i <= precision; i++){
       ai = i * 2 * Math.PI / precision;
       vertices.push(+(Math.sin(ai) * Math.sin(aj)/2).toFixed(6), +(Math.cos(aj)/2).toFixed(6), +(Math.cos(ai) * Math.sin(aj)/2).toFixed(6));
-      uv.push((Math.sin((i/precision))) * 3.5, -Math.sin(j/precision))
+      uv.push(i / precision, 1 - j / precision);
       if(i < precision && j < precision){
         indices.push(p1 = j * (precision + 1) + i, p2 = p1 + (precision + 1), (p1 + 1), (p1 + 1), p2, (p2 + 1));
       }
