effc3434d7aa24c33df3c45cf9d4ddc2c250f716
kohunglee <2528852314@qq.com>
2025-12-03 20:51:29 +0800

    feat: what?
    怎么没同步到 github ？

--- memo/不要驼背.py
+++ memo/不要驼背.py
@@ -3,4 +3,4 @@ while True:
     os.system('osascript -e \'display dialog "不要驼背！不要驼背！！不要驼背！！！不要跑神！" with title "notice" buttons {"OK"} default button 1\'')
     delay = random.randint(60 * 5, 60 * 10)  # 5 ~10 分钟（单位：秒）
     print(f"下次提醒将在 {delay/60:.1f} 分钟后")
-    time.sleep(delay)
+    time.sleep(delay) # test
