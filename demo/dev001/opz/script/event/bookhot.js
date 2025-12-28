18ebe2733c4a8b7de983dc7c3b896086a6294d5d
kohunglee <2528852314@qq.com>
2025-12-28 19:41:20 +0800

    add: dev001

---
+++ demo/dev001/opz/script/event/bookhot.js
@@ -0,0 +1,93 @@
+/**
+ * 当光标选中书时的事件
+ */
+
+const bookHot = {
+
+    typeMap : {
+        2: 3,
+        3: 4,
+        4: 5,
+        5: 5
+    },
+
+    // 新窗口跳转到指定 url 地址（目前先试试，如果热点无事件，则脱离第一视角）
+    jumpUrl : (url) => {
+        // if ('pointerLockElement' in document || 
+        //     'mozPointerLockElement' in document || 
+        //     'webkitPointerLockElement' in document) {
+        //         const exitLock = document.exitPointerLock || 
+        //                          document.mozExitPointerLock || 
+        //                          document.webkitExitPointerLock;
+        //         if (exitLock) {
+        //             exitLock.call(document);
+        //         }
+        // }
+        k.centerDot.closePoint(k);
+    },
+
+    // 在屏幕左上角显示信息
+    showData : (data, isclear = false) => {
+        if(isclear){
+            return document.getElementById('bookInfoLT').innerHTML = '';
+        }
+        const buildInfoHTML = arr => `
+            <button class="collapse-btn" id="collapseBookInfo">${(k.collapseBookInfo) ? '展开' : '折叠'}</button>
+            <table border="1" cellspacing="0" id="bookInfo" cellpadding="4" ${(k.collapseBookInfo) ? 'hidden' : ''}>
+                <tr><th align="right">排名</th><td>${arr[0]}</td></tr>
+                <tr><th align="right">网站</th><td><a href="https://${arr[1]}?refer=ow.ccgxk.com" target="_blank">${arr[1]}</a></td></tr>
+                <tr><th align="right">名称</th><td>${arr[2]}</td></tr>
+                <tr><th align="right">英文</th><td>${arr[3]}</td></tr>
+                <tr><th align="right">简介</th><td>${arr[4]}</td></tr>
+                <tr><th align="right">类型</th><td>${arr[5]}</td></tr>
+            </table>
+            `;
+        document.getElementById('bookInfoLT').innerHTML = buildInfoHTML(data);
+        document.getElementById('collapseBookInfo').addEventListener('click', () => {
+            const ishidden = document.getElementById('bookInfo').hidden;
+            if(ishidden){
+                document.getElementById('collapseBookInfo').innerHTML = '折叠';
+            } else {
+                document.getElementById('collapseBookInfo').innerHTML = '展开';
+            }
+            document.getElementById('bookInfo').hidden = !ishidden;
+            k.collapseBookInfo = !ishidden;
+        })
+    },
+
+    // 获取书本的信息
+    getInfo : (index) => {
+        const args = k.indexToArgs.get(index);
+        let book;
+        if(args?.book){
+            book = args.book;
+        } else {
+            return [];
+        }
+        const y = args.Y;
+        const shelfId = book._shelf;
+        let bookIndex = book._index;
+
+        if(!book) {return 0}
+
+        let tp;  // 通过线索，解决 TP，计算出 realIndex
+        if (book._type === 1) {
+            if(y < 2){
+                tp = 2;
+                bookIndex = bookIndex - 681;
+            } else {
+                tp = 1;
+            }
+        } else {
+            tp = bookHot.typeMap[book._type];
+        }
+        const realIndex = testInShelf.firstFloorIndexTras(tp, bookIndex);
+
+        if (shelfId === k.bookS.floor1.dire4[4]) {
+            const data = testInShelf.floor1dire4_4(realIndex);
+            return data;
+        }
+
+        return [];
+    }
+}
---
+++ demo/dev001/opz/script/init/makegroundmvp.js
@@ -0,0 +1,218 @@
+/**
+ * 创建地面、人物、天空盒
+ */
+function makeGroundMvp(){
+    // 主题变量
+    globalThis.greenStone = k.textureMap.get('greenStone');
+    globalThis.greenStoneborder = k.textureMap.get('greenStoneborder');
+    globalThis.frosted = k.textureMap.get('frosted');
+    globalThis.jpflag = k.textureMap.get('jpflag');
+    globalThis.checkerboard = k.textureMap.get('checkerboard');
+
+    var lastPos = k?.lastPos || {x:21 + Math.random() * 10, y:5.00, z:15 + Math.random() * 10, rX:0, rY:0, rZ:0};
+    k.keys.turnRight = lastPos.rY;
+    const mainVPSize = k?.mainVPSize || 0.5;
+
+    // 添加地面
+    if(true){
+        const X = 0, Y = -0.5 - 2, Z = 0;
+        const width = 2500, depth = 2500, height = 6;
+        k.groundObj = k.addPhy({  // 地面物理体
+            name: 'ground',
+            colliGroup: 1,
+            X: X, Y: Y, Z: Z,
+            width: width, depth: depth, height: height,
+            mass: 0,
+        });
+
+        // plane 地面
+        if(false){
+            const groundSurfaceY = 0.5; 
+            const planeShape = new CANNON.Plane();
+
+            // 3. 创建刚体
+            const groundBody = new CANNON.Body({
+                mass: 0, // 0 = 静态 (STATIC)，不会动
+                position: new CANNON.Vec3(0, groundSurfaceY, 0), // 直接设置表面高度
+                shape: planeShape,
+                material: k.cannonDefaultContactMaterial || null 
+            });
+            groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+            groundBody.collisionFilterGroup = 1;
+            if (k.stoneGroupNum && k.allGroupNum) {
+                groundBody.collisionFilterMask = k.stoneGroupNum | k.allGroupNum;
+            } else {
+                groundBody.collisionFilterMask = -1; // 与一切碰撞
+            }
+            k.world.addBody(groundBody);
+            k.groundObj = {
+                name: 'ground',
+                body: groundBody,
+                width: 99999, depth: 99999, height: 0 
+            };
+        }
+
+        k.W.cube({  // 地面渲染
+            n: 'ground',
+            w: width, d: depth, h: height,
+            x: X, y: Y, z: Z,
+            t: greenStoneborder, b: '#96A48B', mix: 0.6, tile: [width, width],
+        });
+    }
+
+    // 天空盒的渲染和生成
+    if(true) {
+        const c = document.createElement('canvas');
+        const ctx = c.getContext('2d');
+        c.width = 1024;
+        c.height = 512;
+        let g = ctx.createLinearGradient(0, 0, 0, c.height);  // 1. 背景主渐变（纯蓝 → 淡蓝）
+        g.addColorStop(0.00, '#4fa9ff');   // 顶部：纯蓝，极点不易失真
+        g.addColorStop(0.17, '#6ec3ff');   // 中段
+        g.addColorStop(0.35, '#a9e0ff');   // 接近地平线
+        g.addColorStop(0.50, '#eff6fbff');   // 底部：白（云带）
+        ctx.fillStyle = g;
+        ctx.fillRect(0, 0, c.width, c.height);
+        const skyTexture = new Image();  // 输出 img，用于 WebGL
+        skyTexture.src = c.toDataURL();
+        skyTexture.onload = () => {  // 天空盒测试
+            k.W.sphere({
+                n:'skybox_test',
+                y: 25,
+                x: 0,
+                z: 0,
+                rz:0,
+                size: 2500,
+                uncullface: 1,
+                t: skyTexture,
+                rx: 10,
+                ns: true,
+            });
+        };
+    }
+
+    // 添加主角
+    if(true){
+        const mvpIndex = k.addPhy({  // 创建一个立方体，并设置为主角
+            name: 'mainPlayer',
+            DPZ : 0,
+            colliGroup: 1,
+            X:lastPos.x, Y:lastPos.y + 1, Z:lastPos.z,
+            mixValue:0.7,
+            size: mainVPSize,
+            mass: 50,
+            background : '#333',
+            texture: greenStone,
+        });
+        k.mainVPlayer = mvpIndex;
+        if(k?.centerDot){
+            k.centerDot.setCamView(2);  // 设置默认视角 类型2
+        }
+    }
+
+    // 配置默认的运动速度
+    if(true){
+        k.SPRINT_MIN_SPEED = 5;
+        k.SPRINT_MAX_SPEED = 15.5;
+    }
+
+    const orig_jumpYVel = k.jumpYVel;
+    const orig_jumpHoldLimit = k.JUMP_HOLD_LIMIT;
+    const mvp = k.mainVPlayer;
+    const mp = k.mainVPlayer.body.position;
+    const mk = k.keys;
+    k.lastHotBookData = [];  // 用于记录上一次的热点图书信息，方便判断
+    let Last_mvpBodyPos = (mp.x + mp.y + mp.z + mk.turnRight).toFixed(2);  // 用于判断主角是否停下脚步的辅助
+    k.myRestDoFunc = dofunc();  // 静止时要执行的函数队列
+    setInterval(  // 动态调整人物的跳跃、地心引力等，每秒走一遍
+        () => {
+            if(k.W.lastReportTime < 2000){ return 0 }  // 项目启动前 2s 不运行，防止看起来卡顿
+
+            // 不同区域的跳跃和速度不一样的实现逻辑
+            if(true){
+                const x = mvp.X;
+                const z = mvp.Z;
+                const y = mvp.Y;
+                if(x < 52.3 && x > 17.1 && z < -15 && z > -45.1) {
+                    if(x < 50.5  && x > 19.1 && z < -24.9 && z > -35.5){  // 在大厅以及其他区域
+                        k.WALK_SPEED = 1/20;  //+ 走路使用快速度
+                        k.jumpYVel = orig_jumpYVel;  //+ 跳跃为原力度
+                        k.JUMP_HOLD_LIMIT = orig_jumpHoldLimit;
+                        k.world.gravity.set(0, -9.82, 0);
+                    } else if(y < 17) {  // 在图书区
+                        k.WALK_SPEED = 1/40;  //+ 走路使用慢速度
+                        k.jumpYVel = 0.8;  //+ 减弱跳跃力度
+                        k.JUMP_HOLD_LIMIT = 0.5;
+                        k.world.gravity.set(0, -9.82/4, 0);
+                    }
+                } else {
+                    k.WALK_SPEED = 1/20;  //+ 走路使用快速度
+                    k.jumpYVel = orig_jumpYVel;  //+ 跳跃为原力度
+                    k.JUMP_HOLD_LIMIT = orig_jumpHoldLimit;
+                    k.world.gravity.set(0, -9.82, 0);
+                }
+            }
+
+            // 保证人物不掉地面
+            if(true){
+                if(k.mainVPlayer.body.position.y < 0){
+                    k.mainVPlayer.body.position.y = 50;
+                }
+            }
+
+            // 防止在 F 冻结模式，碰障碍物后失重移动
+            if(true){
+                const mvpBody = k.mainVPlayer.body;
+                if(mvpBody.mass === 0){
+                    mvpBody.velocity.set(0, 0, 0);  // 设置线速度为0
+                    mvpBody.angularVelocity.set(0, 0, 0);  // 设置角速度为0
+                    mvpBody.force.set(0, 0, 0);  // 清除所有作用力
+                    mvpBody.torque.set(0, 0, 0);  // 清除所有扭矩
+                }
+            }
+
+            // 在人物运动、静止时执行的函数
+            if(true){
+                if((mp.x + mp.y + mp.z + mk.turnRight).toFixed(2) === Last_mvpBodyPos) {  // 人物处于静止状态
+                    k.myRestDoFunc();
+                } else {  // 人物处于运动状态
+                    Last_mvpBodyPos = (mp.x + mp.y + mp.z + mk.turnRight).toFixed(2);
+                }
+            }
+
+            // 光标划过书本，在屏幕左上角，显示书本的信息
+            if(true){
+                if (k.hotPoint) {
+                    const data = bookHot.getInfo(k.hotPoint);
+                    const last = k.lastHotBookData ?? [];
+                    const hasNewData = data.length > 0 && data !== last;
+                    const needClear  = data.length !== last.length;
+                    if (hasNewData) {
+                        bookHot.showData(data);  // 左上角展示
+                    } else if (needClear) { bookHot.showData([], true); }  // 关闭左上角信息
+                    k.lastHotBookData = data;
+                }
+            }
+        }
+    , 100);
+}
+
+/**
+ * 任务队列模式
+ * ------------
+ * 主要用于在人物静止时加载 svg ，防止画面卡帧、卡顿
+ */
+function dofunc() {
+  let q = [];
+  function run() {
+    const a = q;
+    q = [];
+    for (let i = 0; i < a.length; i++) a[i]();
+  }
+  run.add = f => q[q.length] = f;
+  return run;
+}
+
+
+
+
