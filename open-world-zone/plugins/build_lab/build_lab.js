eeac0e577ead536935904d69fc44cf88619b1aea
kohunglee <2528852314@qq.com>
2026-03-29 19:27:57 +0800

    feat: add signboard
    先把板子弄好了，现在开始研究怎么能让它显示字

--- open-world-zone/plugins/build_lab/build_lab.js
+++ open-world-zone/plugins/build_lab/build_lab.js
@@ -11,7 +11,8 @@ const COLORS = {
 
 const INDICES = {  // 不同物体的索引
     floor: [4, 6, 0, 2, 3, 7, 19, 17, 22, 21, 18, 20, 44, 43, 42, 45, 39, 40, 38, 37, 35, 36, 102],
-    decorations: [49, 48, 91, 92, 47]
+    decorations: [49, 48, 47],
+    signBoard: [107, 108],
 };
 
 export default function(ccgxkObj) {
@@ -28,7 +29,7 @@ export default function(ccgxkObj) {
 
         symer.offset([93], -0.9, 8, 'x', -0.48, 'y');  // 阵列台阶
 
-        const arrB = [];  //+ 提取地板坐标
+        const arrB = [];  //+ 提取地板属性到 arrB
         INDICES.floor.forEach(i => {
             if (insts[i]) {
                 arrB.push({ ...insts[i] });
@@ -36,16 +37,35 @@ export default function(ccgxkObj) {
             }
         });
 
-        ccgxkObj.dataProc.process({
+        ccgxkObj.dataProc.process({  //+ 显示 arrB 地板
             data: arrB,
             name: 'build_lab_stage',
             type: 2,
             texture: paper02,
             mixValue: 0.8,
         });
+
+        const arrC = [];  //+ 提取信息板属性到 arrC
+        INDICES.signBoard.forEach(i => {
+            if (insts[i]) {
+                insts[i].dz = 3;
+                arrC.push({ ...insts[i] });
+                insts[i] = { "del": 1 };
+            }
+        });
+
+        ccgxkObj.dataProc.process({  //+ 显示 arrC 信息板
+            data: arrC,
+            name: 'build_lab_signBoard',
+            type: 2,
+            texture: paper02,
+            model: 'plane',
+            mixValue: 0.8,
+            invisible: false, noIns: true,  // 纹理专用
+        });
     }
 
-    k.visCubeLen = insts.length - 1;
+    k.visCubeLen = insts.length - 1;  //+ 建造器相关
     for (let i = 0; i < 9990; i++) {
         insts.push({
             x: 1e9, y: 1e9, z: 1e9,
@@ -54,7 +74,7 @@ export default function(ccgxkObj) {
         });
     }
 
-    const idx = ccgxkObj.dataProc.process({
+    const idx = ccgxkObj.dataProc.process({  //+ 渲染 0~1w
         data: insts,
         name: 'build_lab',
         type: 1,
@@ -62,8 +82,6 @@ export default function(ccgxkObj) {
         mixValue: 0.8,
     });
 
-    const rootArgs = k.indexToArgs.get(idx);  //+ 设置建造器
-    if (rootArgs) {
-        k.wBuildInstName = `sk_${idx}_${rootArgs.dataName}`;
-    }
+    const rootArgs = k.indexToArgs.get(idx);  //+ 设置建造器的操纵 W ID
+    if (rootArgs) {  k.wBuildInstName = `sk_${idx}_${rootArgs.dataName}`; }
 }
