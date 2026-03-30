84f72d30b7b4cbf24faeeaed2c05be99f7b95d3a
kohunglee <2528852314@qq.com>
2026-03-30 15:45:22 +0800

    feat: glm, add the admin html
    让质谱给我写了个 admin ，是一个平面配套网站。

---
+++ open-world-zone/plugins/build_lab/signsData.js
@@ -0,0 +1,45 @@
+/**
+ * 信息板数据配置
+ * 由 admin.html 自动生成
+ */
+export default {
+  "version": 1,
+  "boards": [
+    {
+      "id": "testSign1",
+      "name": "左侧墙面",
+      "mode": "text",
+      "content": "野狗不需要墓碑，奔跑到腐烂即可。啊啊啊！"
+    },
+    {
+      "id": "testSign2",
+      "name": "右侧墙面",
+      "mode": "text",
+      "content": "这是一段测试文本。三维空间适合做长期结构化知识的栖息地，而不是每一条碎片笔记的唯一入口。"
+    },
+    {
+      "id": "nouse",
+      "name": "备用墙面",
+      "mode": "text",
+      "content": "欢迎来到数字禅修空间。在这里，你可以慢慢逛，慢生活。"
+    },
+    {
+      "id": "testSign3",
+      "name": "装饰圆",
+      "mode": "canvas",
+      "content": "drawCircle"
+    },
+    {
+      "id": "testSign5",
+      "name": "装饰叉",
+      "mode": "canvas",
+      "content": "drawCross"
+    },
+    {
+      "id": "testSign4",
+      "name": "装饰画",
+      "mode": "image",
+      "content": "https://i.mji.rip/2026/03/29/922312a08e4c5b2ada1d53a700e06d04.jpeg"
+    }
+  ]
+};
