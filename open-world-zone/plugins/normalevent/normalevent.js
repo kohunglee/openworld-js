82901c7371cec6c27082a3d1f8e17c769578ee57
kohunglee <2528852314@qq.com>
2026-04-03 21:03:47 +0800

    fix

--- open-world-zone/plugins/normalevent/normalevent.js
+++ open-world-zone/plugins/normalevent/normalevent.js
@@ -7,6 +7,8 @@ export default function(ccgxkObj) {
     // console.log('导入自己的 常用快捷键事件 插件成功');
     const myevent = {
         keyEvent : (e, ccgxkObj) => {
+            if(document.getElementById('signPanelModal')&&!document.getElementById('signPanelModal').hidden)return;
+            if(document.getElementById('myHUDModal')&&!document.getElementById('myHUDModal').hidden)return;
             const key = e.key.toLowerCase();
 
             if(key === 'e' || key === ' ') {  // 在冻结物体情况下，按 e 键或空格键，可以解除冻结
@@ -15,19 +17,6 @@ export default function(ccgxkObj) {
                     mvpBody.mass = 50;  // 重量还原
                 }
             }
-
-            // if(key === 'f') {  // 键盘上的 f 键被按下（冻结物体）
-            //     const mvpBody = ccgxkObj.mainVPlayer.body;
-            //     if(mvpBody.mass === 0){
-            //         mvpBody.mass = 50;  // 重量还原
-            //     } else {
-            //         mvpBody.mass = 0;  // 重量归 0
-            //         mvpBody.velocity.set(0, 0, 0);  // 设置线速度为0
-            //         mvpBody.angularVelocity.set(0, 0, 0);  // 设置角速度为0
-            //         mvpBody.force.set(0, 0, 0);  // 清除所有作用力
-            //         mvpBody.torque.set(0, 0, 0);  // 清除所有扭矩
-            //     }
-            // }
 
             if(key === 'v'){  // 切换视角
                 ccgxkObj.centerDot.setCamView();
@@ -73,6 +62,9 @@ export default function(ccgxkObj) {
             const key = event.key.toLowerCase();
             const mvpBody = ccgxkObj?.mainVPlayer?.body;
 
+            if(document.getElementById('signPanelModal')&&!document.getElementById('signPanelModal').hidden)return;
+            if(document.getElementById('myHUDModal')&&!document.getElementById('myHUDModal').hidden)return;
+
             // 没有物理体就直接退出
             if (!mvpBody) return;
 
@@ -80,7 +72,7 @@ export default function(ccgxkObj) {
             if (key === 'e') {
                 // 只有当前是冻结状态，才执行解冻
                 if (mvpBody.mass === 0) {
-                mvpBody.mass = 50;
+                    mvpBody.mass = 50;
                 }
             }
 
